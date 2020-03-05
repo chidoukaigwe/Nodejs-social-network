28cdd8952cb3b9ac1dbbbeb1d9933ee568fe80c6
Chido Ukaigwe <chidodesigns@gmail.com>
2020-03-05 20:19:19 +0000

    sanitized HTML

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import DOMPurify from 'dompurify'
 
 export default class Search {
 
@@ -62,7 +63,7 @@ export default class Search {
 
      renderResultsHTML(posts) {
         if (posts.length) {
-          this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
+          this.resultsArea.innerHTML = DOMPurify.sanitize(`<div class="list-group shadow-sm">
           <div class="list-group-item active"><strong>Search Results</strong> (${posts.length > 1 ? `${posts.length} items found` : '1 item found'})</div>
 
             ${posts.map(post => {
@@ -73,7 +74,7 @@ export default class Search {
             </a>`
             }).join('')}
     
-          </div>`
+          </div>`)
         } else {
           this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search</p>`
         }
