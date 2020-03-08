033fa6db643e5f6e1582bb64604b532edc2d8065
Chido Ukaigwe <chidodesigns@gmail.com>
2020-03-08 14:30:14 +0000

    live validation form complete

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,8 +1,25 @@
 //Async Requests
 import axios from 'axios'
 
+//Import Search File
 import Search from './modules/search'
+
+//Import Registration Form 
+import RegistrationForm from './modules/registrationForm'
+
+// Import Chat File 
+import Chat from './modules/chat'
 
 if (document.querySelector('.header-search-icon')) {
     new Search()
+}
+
+if (document.querySelector('#chat-wrapper')) {
+    new Chat()
+}
+
+if (document.querySelector('#registration-form')) {
+
+    new RegistrationForm()
+
 }
