9256d6ab49d5b61a78ef18d5dde8a851a603bc97
Chido Ukaigwe <chidodesigns@gmail.com>
2020-03-21 10:48:34 +0000

    Completed basic API and set up CORS policy for any domain

--- controllers/userController.js
+++ controllers/userController.js
@@ -235,3 +235,13 @@ exports.profileFollowersScreen = async function (req, res) {
       res.json('Sorry you must provide a valid token')
     }
   }
+
+  exports.apiGetPostsByUsername = async function (req, res) {
+    try {
+      let authorDoc = await User.findByUsername(req.params.username)
+      let posts = await Post.findByAuthorId(authorDoc._id)
+      res.json(posts)
+    } catch (error) {
+      res.json("Sorry invalid user request")
+    }
+  }
