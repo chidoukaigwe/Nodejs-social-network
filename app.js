2ed78240ff655632f3e9a2974ed166982ae3899d
Chido Ukaigwe <chidodesigns@gmail.com>
2020-03-21 10:22:00 +0000

    created a basic api with JWT tokens

--- app.js
+++ app.js
@@ -11,7 +11,18 @@ const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
 const markdown = require('marked')
 const sanitizeHTML = require('sanitize-html')
+
 const app = express()
+
+//  [ two most common ways of accepting data across the web ]
+//  Boilerplate Code add user submitted data onto our request object so we can access it via req.body
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// API Router  - As this line of code is above the app.use() function that will bring in a bunch of code - this API router does not have to apply those functions calls within the app.use main function, therefore making the route lighter
+app.use('/api', require('./router-api'))
+
+
 
 //  Boilerplate Config Code
 let sessionOptions = session({
@@ -55,11 +66,6 @@ app.use(function (req, res, next) {
 })
 
 const router = require('./router')
-
-//  [ two most common ways of accepting data across the web ]
-//  Boilerplate Code add user submitted data onto our request object so we can access it via req.body
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
 
 //  Make public files accessible (css/browserJS)
 app.use(express.static('public'))
